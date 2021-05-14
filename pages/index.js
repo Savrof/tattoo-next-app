1c5328a31e172f67c5abfe4d22f8a22a36ffbd1c
Savrof <savro123@yandex.ru>
2021-05-14 22:01:36 +0800

    Добавил картинки через компонент next

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import MainLayer from "../layout/MainLayer"
 import styles from "../styles/Home.module.css";
+import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -7,9 +8,33 @@ export default function Home() {
             <div>
                 <div className={styles.container}>
                     <main className={styles.main}>
-                        <div className={styles.block}></div>
-                        <div className={styles.block}>block</div>
-                        <div className={styles.block}>block</div>
+                        <div className={styles.block}>
+                            <Image 
+                                src="/../public/main-1.jpg"
+                                alt="Picture of tattoo work"
+                                layout="responsive"
+                                width={850}
+                                height={300}
+                                />
+                        </div>
+                        <div className={styles.block}>
+                            <Image 
+                                src="/../public/main-2.jpg"
+                                alt="#"
+                                layout="responsive"
+                                width={840}
+                                height={300}
+                                />
+                        </div>
+                        <div className={styles.block}>
+                            <Image  
+                                src="/../public/main-3.jpg"
+                                alt="#"
+                                layput="responsive"
+                                width={850}
+                                height={300}
+                                />
+                        </div>
                     </main>
                 </div>
             </div>
